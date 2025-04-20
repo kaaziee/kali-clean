73031ad4354762b2286126dfa55ec0dc298c8067
kaazie <153534203+kaaziee@users.noreply.github.com>
2025-04-20 15:42:22 +0200

    Update install.sh
    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 
+# Update and upgrade the system
 sudo apt update && sudo apt upgrade -y
 
+# Install necessary packages
 sudo apt-get install -y wget curl git thunar
 sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
 sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
 sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
 
+# Create font directory and install fonts
 mkdir -p ~/.local/share/fonts/
-
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Iosevka.zip
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip
-
 unzip Iosevka.zip -d ~/.local/share/fonts/
 unzip RobotoMono.zip -d ~/.local/share/fonts/
-
 fc-cache -fv
 
+# Install Alacritty
 wget https://github.com/barnumbirr/alacritty-debian/releases/download/v0.10.0-rc4-1/alacritty_0.10.0-rc4-1_amd64_bullseye.deb
 sudo dpkg -i alacritty_0.10.0-rc4-1_amd64_bullseye.deb
 sudo apt install -f
 
+# Install i3-gaps
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps && mkdir -p build && cd build && meson ..
 ninja
 sudo ninja install
 cd ../..
 
+# Install pywal
 pip3 install pywal
 
+# Create necessary directories for configuration
 mkdir -p ~/.config/i3
 mkdir -p ~/.config/compton
 mkdir -p ~/.config/rofi
@@ -42,7 +46,19 @@ cp .fehbg ~/.fehbg
 cp .config/i3/clipboard_fix.sh ~/.config/i3/clipboard_fix.sh
 cp -r .wallpaper ~/.wallpaper 
 
+# Create wallpaper script to set wallpaper
+echo "#!/bin/sh" > ~/.wallpaper/wallpaper-script.sh
+echo "feh --no-fehbg --bg-scale ~/.wallpaper/23.jpg" >> ~/.wallpaper/wallpaper-script.sh
+
+# Make the wallpaper script executable
+chmod +x ~/.wallpaper/wallpaper-script.sh
+
+# Add wallpaper script to startup by creating or modifying ~/.xprofile
+echo '#!/bin/sh' > ~/.xprofile
+echo 'sh ~/.wallpaper/wallpaper-script.sh' >> ~/.xprofile
+
 echo "Done! Grab some wallpaper and run pywal -i filename to set your color scheme. To have the wallpaper set on every boot edit ~.fehbg"
 echo "After reboot: Select i3 on login, run lxappearance and select arc-dark"
 
+# Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
